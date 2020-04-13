519770c8d3d2560967e7caf80efc5bdb3858bc83
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2020-04-12 20:56:08 -0500

    fix user admin bug

--- stl_dsa/users/admin.py
+++ stl_dsa/users/admin.py
@@ -12,6 +12,6 @@ class UserAdmin(auth_admin.UserAdmin):
 
     form = UserChangeForm
     add_form = UserCreationForm
-    fieldsets = (("User", {"fields": ("name",)}),) + auth_admin.UserAdmin.fieldsets
+    fieldsets = (("User", {"fields": ("first_name",'last_name')}),) + auth_admin.UserAdmin.fieldsets
     list_display = ["username", "first_name", "last_name", "is_superuser"]
     search_fields = ["first_name", "last_name"]
