ba0555ca58f531a85004951d2cad0e902fd1e354
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2020-05-11 19:15:28 -0500

    change redirect argument in redirect view

--- stl_dsa/users/views.py
+++ stl_dsa/users/views.py
@@ -43,7 +43,7 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
     permanent = False
 
     def get_redirect_url(self):
-        return reverse("users:detail", kwargs={"username": self.request.user.username})
+        return reverse("users:detail", kwargs={"id": self.request.user.id})
 
 
 user_redirect_view = UserRedirectView.as_view()
