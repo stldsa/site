54a301112cc0b16f00ed28d8b76a494266f8c8f7
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2020-07-20 19:04:42 -0500

    temp change to upload new committee events

--- events/views.py
+++ events/views.py
@@ -8,17 +8,15 @@ from django.conf import settings
 # Create your views here.
 
 def update_events():
-    # print(APICalls.objects.order_by('-datetime')[0])
     last_api_call = APICalls.objects.order_by('-datetime')[0]
     last_api_call = last_api_call.datetime.isoformat()
     events = []
     for key in settings.ACTIONNETWORK_API_KEYS:
         response = requests.get(
             'https://actionnetwork.org/api/v2/events/', 
-            params = {'filter': f'modified_date gt \'{last_api_call}\''},
+            # params = {'filter': f'modified_date gt \'{last_api_call}\''},
             headers = {'OSDI-API-Token': key},
         )
-
         tz = pytz.timezone('America/Chicago')
         chicago_now = datetime.now(tz)
         APICalls.objects.all().delete()
