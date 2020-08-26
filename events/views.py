90011e98da4f5aa9c6168363aa7b998c4947dc49
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2020-08-26 16:21:44 -0500

    reverse events order

--- events/views.py
+++ events/views.py
@@ -41,6 +41,6 @@ class EventList(ListView):
 
     def get_queryset(self):
         update_events()
-        return Event.objects.filter(date__gte=datetime.today().date())
+        return Event.objects.filter(date__gte=datetime.today().date()).reverse()
 
 event_list = EventList.as_view()
