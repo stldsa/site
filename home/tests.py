07bd57f9f517b35191195358bd618de4dec98ecb
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2020-10-09 19:31:48 -0500

    setup django tests for home page

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver import FirefoxOptions
 
 # Create your tests here.
+
+opts = FirefoxOptions()
+opts.add_argument("--headless")
+
+
+class PrioritiesTestCase(LiveServerTestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox(options=opts)
+
+    def tearDown(self):
+        self.browser.quit()
