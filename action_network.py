33a20461705372221983d5ed8fcd60ef3b58a84e
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2021-02-21 00:26:51 -0600

    action network module fix

--- action_network.py
+++ action_network.py
@@ -4,10 +4,10 @@ import json
 
 
 def get_events():
-    events = requests.get(
+    events_response = requests.get(
         "https://actionnetwork.org/api/v2/events/",
         # params={"filter": f"modified_date gt '{last_api_call}'"},
         headers={"OSDI-API-Token": os.getenv("AN_API_KEY")},
     )
-    events_json = json.loads(events.content)
+    events_json = json.loads(events_response.content)
     return events_json["_embedded"]["osdi:events"]
