6eeb68a48a5fdceae2be1e2fe431e5d075b3b0b8
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2021-03-11 02:38:40 -0600

    revert db settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -41,17 +41,17 @@ LOCALE_PATHS = [ROOT_DIR.path("locale")]
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
-# DATABASES = {"default": env.db("DATABASE_URL", default="postgres:///stl_dsa")}
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
-    }
-}
+DATABASES = {"default": env.db("DATABASE_URL", default="postgres:///stl_dsa")}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'db',
+#         'PORT': 5432,
+#     }
+# }
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
 # URLS
