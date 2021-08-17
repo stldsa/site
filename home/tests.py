955c63e8cce2c4133e7d7531a9ace5eeb066442b
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2021-08-17 00:42:23 -0500

    more cleanup

--- home/tests.py
+++ home/tests.py
@@ -1,16 +1,2 @@
-import sys
-import os
-from django.test import LiveServerTestCase
-from selenium import webdriver
-from selenium.webdriver import FirefoxOptions
-
-opts = FirefoxOptions()
-opts.add_argument("--headless")
-
-
-class PrioritiesTestCase(LiveServerTestCase):
-    def setUp(self):
-        self.browser = webdriver.Firefox(options=opts)
-
-    def tearDown(self):
-        self.browser.quit()
+def test_priority_committees_in_menu():
+    pass
