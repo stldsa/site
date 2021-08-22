d77a2b4d85dfde66f2699d099465559e0a3e6fb9
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2021-08-21 23:08:50 -0500

    remove events conftest

--- events/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-
-@pytest.fixture(scope="module")
-def vcr_config(vcr_config):
-    return {
-        "filter_headers": [("OSDI-API-Token", "abcdefghijklmnopqrstuvwxyz123456")],
-    }
