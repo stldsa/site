519fe8cc32f78946382d5fd513d79ece29e9335a
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2021-09-03 01:16:39 -0500

    move an sync command to events app

--- actionnetwork/management/commands/sync-action-network.py
+++
@@ -1,9 +0,0 @@
-from actionnetwork import action_network as an
-from django.core.management.base import BaseCommand
-
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        events = an.get_events()
-        for event in events:
-            an.save_events(event)
