448b249ec201e941d26a9bd1dedd5c19aa400a82
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2021-09-03 00:40:35 -0500

    dont create chair group if already exists

--- committees/signals/handlers.py
+++ committees/signals/handlers.py
@@ -7,6 +7,6 @@ from django.contrib.auth.models import Group
 @receiver(page_published, sender=CommitteePage)
 def handler(sender, **kwargs):
     instance = kwargs["instance"]
-    Group.objects.create(
+    Group.objects.get_or_create(
         name=f"{instance.title} {instance.get_formation_type_display()} Chair(s)"
     )
