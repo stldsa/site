060bd0c1417eab865d9836a2e2b11d9125a7ee57
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2022-01-10 12:33:32 -0600

    redesign myDSA page

---
+++ stl_dsa/test_integrations.py
@@ -0,0 +1,7 @@
+# from stl_dsa.users.models import User
+
+
+# def test_user_is_member(faker):
+#     email = faker.email()
+#     taggings =
+#     user = User(email=email, first_name=faker.first_name(), last_name=faker.last_name())
