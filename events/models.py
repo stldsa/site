706dc159d9106fb1dfa793e525e18e6d5843680c
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2022-06-20 12:11:38 -0500

    sourcery refactor

--- events/models.py
+++ events/models.py
@@ -29,7 +29,7 @@ class Event(models.Model):
     id = models.CharField(max_length=50, primary_key=True)
 
     def __str__(self):
-        return self.title + " " + str(self.start)
+        return f"{self.title} {str(self.start)}"
 
 
 class EventsPage(AbstractLinkPage):
