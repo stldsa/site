c5b54b37b0ffda2ddc60d86afe727f918091cb21
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2022-07-02 20:16:43 -0500

    remove stray code

--- news/apps.py
+++ news/apps.py
@@ -1,19 +1,4 @@
 from django.apps import AppConfig
-from django.conf import settings
-
-from actionnetwork.email import create
-
-
-def create_email(sender, **kwargs):
-    newspage = kwargs["instance"]
-    response = create(
-        newspage.title,
-        newspage.body,
-        "STL DSA",
-        "info@stldsa.org",
-        settings.ACTIONNETWORK_API_KEYS["main"],
-    )
-    print(response.content)
 
 
 class NewsConfig(AppConfig):
