7377c21d8837b68da4a535f69bb2d548e71bd29d
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2022-07-06 14:20:49 -0500

    indent error

--- stl_dsa/utils/s3_cors_put.py
+++ stl_dsa/utils/s3_cors_put.py
@@ -1,7 +1,8 @@
-    import boto3
-    from django.conf import settings
+import boto3
+from django.conf import settings
 
-    s3 = boto3.client("s3")
-    s3.put_bucket_cors(
-        Bucket=settings.AWS_STORAGE_BUCKET_NAME, CORSConfiguration=settings.CORS_CONFIGURATION
-    )
+s3 = boto3.client("s3")
+s3.put_bucket_cors(
+    Bucket=settings.AWS_STORAGE_BUCKET_NAME,
+    CORSConfiguration=settings.CORS_CONFIGURATION,
+)
