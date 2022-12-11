4bc5e61492755c7dcd6c445417e8d37ea99f1ff0
Ty Schlichenmeyer <tyschlich@gmail.com>
2022-12-11 04:31:48 +0000

    configure black

--- stl_dsa/users/views.py
+++ stl_dsa/users/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
 from django.views.generic import DetailView, UpdateView
 from django.contrib import messages
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 User = get_user_model()
 
