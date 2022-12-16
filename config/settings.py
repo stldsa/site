89f63630172d791ca0fb7506201fe48a25406be6
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2022-12-16 00:49:41 +0000

    feed bucket name to django

--- config/settings.py
+++ config/settings.py
@@ -248,6 +248,7 @@ LOCAL_SERVE_MEDIA_FILES = True
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID", default=None)
 AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY", default=None)
+AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME", default=None)
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "0.0.0.0", "127.0.0.1"])
 
