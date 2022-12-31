331537c337d99ddb52e3e2e47e529990351e1ad6
Ty Schlichenmeyer <tyschlich@gmail.com>
2022-12-31 14:21:29 +0000

    rename committees to formations in admin

--- committees/admin.py
+++ committees/admin.py
@@ -1,15 +1,13 @@
-from django.contrib import admin
 from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
-from .models import CommitteePage
+from committees.models import CommitteePage
 
 
 class CommitteeAdmin(ModelAdmin):
     model = CommitteePage
-    menu_label = "Committees"
+    menu_label = "Formations"
     menu_order = 300
     add_to_settings_menu = False
     exclude_from_explorer = False
-    # list_display = ('name','formation_type')
 
 
 modeladmin_register(CommitteeAdmin)
