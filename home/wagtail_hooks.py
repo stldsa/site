5174eb8b141f2180b1dbf00a29cea9850c5c6e03
Ty Schlichenmeyer <tyschlich@gmail.com>
2022-12-31 14:13:19 +0000

    customize admin menu

--- home/wagtail_hooks.py
+++ home/wagtail_hooks.py
@@ -3,5 +3,8 @@ from wagtail import hooks
 
 @hooks.register("construct_main_menu")
 def hide_pages(request, menu_items):
-    print([item for item in menu_items])
-    menu_items[:] = [item for item in menu_items if item.name != "Pages"]
+    menu_items[:] = [
+        item
+        for item in menu_items
+        if item.name not in ["explorer", "documents", "images", "reports"]
+    ]
