3f390ff4593487edd07e56f2af3c4627ee3c06fb
Ty Schlichenmeyer <tyschlich@gmail.com>
2022-12-31 11:54:31 +0000

    stripping i18n

--- config/settings.py
+++ config/settings.py
@@ -134,7 +134,6 @@ TEMPLATES = [
             "context_processors": [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
-                "django.template.context_processors.i18n",
                 "django.template.context_processors.media",
                 "django.template.context_processors.static",
                 "django.template.context_processors.tz",
