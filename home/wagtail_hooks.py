41cd84618141a31ece410e2763d15e2ce5c2722f
Ty Schlichenmeyer <tyschlich@gmail.com>
2023-01-01 02:11:19 +0000

    admin homnepage tweaks

--- home/wagtail_hooks.py
+++ home/wagtail_hooks.py
@@ -8,3 +8,8 @@ def hide_pages(request, menu_items):
         for item in menu_items
         if item.name not in ["explorer", "documents", "images", "reports"]
     ]
+
+
+@hooks.register("construct_homepage_summary_items")
+def hide_homepage_summary_items(request, items):
+    items.clear()
