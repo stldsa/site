988b2eec72cdbb73263c38bcd6fe1a4035bd08de
Ty Schlichenmeyer <tyschlich@gmail.com>
2023-01-02 21:30:51 +0000

    customize admin colors

--- home/wagtail_hooks.py
+++ home/wagtail_hooks.py
@@ -1,4 +1,6 @@
 from wagtail import hooks
+from django.utils.html import format_html
+from django.templatetags.static import static
 
 
 @hooks.register("construct_main_menu")
@@ -13,3 +15,8 @@ def hide_pages(request, menu_items):
 @hooks.register("construct_homepage_summary_items")
 def hide_homepage_summary_items(request, items):
     items.clear()
+
+
+@hooks.register("insert_global_admin_css")
+def global_admin_css():
+    return format_html('<link rel="stylesheet" href="{}">', static("css/wagtail.css"))
