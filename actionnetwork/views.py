75381f2d60d69afe03efbe1175a5ea04493461d2
Ty Schlichenmeyer <tyschlich@gmail.com>
2023-01-04 08:44:47 +0000

    clean up stray prints

--- actionnetwork/views.py
+++ actionnetwork/views.py
@@ -12,14 +12,11 @@ def person_helper(request):
                 "email_addresses": [{"address": request.POST.get("email")}],
             }
         }
-        print(data)
         response = requests.post(
             "https://actionnetwork.org/api/v2/people/",
             json=data,
             headers={"OSDI-API-Token": settings.ACTIONNETWORK_API_KEYS["main"]},
         )
-        print(response.status_code)
-        print(response.text)
         if response.status_code == 200:
             messages.success(request, "You've been added to our email list!")
     return HttpResponseRedirect("/")
