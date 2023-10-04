ffe2412846b55abce216c2b027825930d29568b3
Ty Schlichenmeyer <ty.schlich@gmail.com>
2023-10-04 08:21:53 -0500

    restore full admin menu

--- home/wagtail_hooks.py
+++ home/wagtail_hooks.py
@@ -3,15 +3,6 @@ from django.utils.html import format_html
 from django.templatetags.static import static
 
 
-@hooks.register("construct_main_menu")
-def hide_pages(request, menu_items):
-    menu_items[:] = [
-        item
-        for item in menu_items
-        if item.name not in ["explorer", "documents", "images", "reports"]
-    ]
-
-
 @hooks.register("construct_homepage_summary_items")
 def hide_homepage_summary_items(request, items):
     items.clear()
