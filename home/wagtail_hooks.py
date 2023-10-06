b139cfa43b06a7b746b9fc73252784ebb8f7f19a
Ty Schlichenmeyer <ty.schlich@gmail.com>
2023-10-06 04:39:42 +0000

    remove wagtail hook for clearing main item summary - not working

--- home/wagtail_hooks.py
+++ home/wagtail_hooks.py
@@ -3,11 +3,6 @@ from django.utils.html import format_html
 from django.templatetags.static import static
 
 
-@hooks.register("construct_homepage_summary_items")
-def hide_homepage_summary_items(request, items):
-    items.clear()
-
-
 @hooks.register("insert_global_admin_css")
 def global_admin_css():
     return format_html('<link rel="stylesheet" href="{}">', static("css/wagtail.css"))
