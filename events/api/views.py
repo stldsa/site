ac4e31c09cfb20836010ed366ba329a23a550976
Ty Schlichenmeyer <ty.schlich@gmail.com>
2023-10-10 03:05:40 +0000

    fix: timezone-aware event datetimes

--- events/api/views.py
+++ events/api/views.py
@@ -1,14 +1,16 @@
 from events.api.serializers import EventSerializer
 from rest_framework import viewsets
 from events.models import Event
+from django.utils.dateparse import parse_datetime
+from django.utils.timezone import make_aware
 
 
 class EventViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = EventSerializer
 
     def get_queryset(self):
-        start = self.request.query_params.get("start")
-        end = self.request.query_params.get("end")
+        start = make_aware(parse_datetime(self.request.query_params.get("start")))
+        end = make_aware(parse_datetime(self.request.query_params.get("end")))
         return (
             Event.objects.filter(start__range=(start, end))
             .exclude(title__icontains="members only")
